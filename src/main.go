de6b92be6aebab525aa3d747826c03b6c5539247
pedro-git-projects <pedro.coding.contact@gmail.com>
2024-10-26 16:34:04 -0300

    [feat] marshaling investigators to json and serving via http

--- src/main.go
+++ src/main.go
@@ -1,54 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 
-	"github.com/pedro-git-projects/necronomicon-engine/src/db"
-	"github.com/pedro-git-projects/necronomicon-engine/src/investigator"
+	"github.com/pedro-git-projects/necronomicon-engine/src/api"
 )
 
 func main() {
-	err := db.InitializeDB("./db/investigators.db")
+	http.HandleFunc("/investigator", api.InvestigatorHandler)
+
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		log.Fatal("Failed to initialize database:", err)
+		log.Fatal("Server failed:", err)
 	}
-	defer db.CloseDB()
-
-	i := &investigator.Investigator{
-		Info: investigator.Info{
-			Name:       "John Doe",
-			Player:     "Alice",
-			Occupation: "Detective",
-			Age:        35,
-			Sex:        investigator.Male,
-			Residence:  "Arkham",
-			Birthplace: "Boston",
-		},
-		Luck: 50,
-		MP:   10,
-		Wealth: investigator.Wealth{
-			SpendingLevel: 1000,
-			Cash:          200,
-			Assets:        5000,
-		},
-	}
-
-	i.InitializeTwenties()
-
-	fmt.Println("Original Investigator:")
-	i.Print()
-
-	err = i.Save()
-	if err != nil {
-		log.Fatal("Failed to save investigator:", err)
-	}
-
-	loadedInvestigator, err := investigator.LoadInvestigator(1)
-	if err != nil {
-		log.Fatal("Failed to load investigator:", err)
-	}
-
-	fmt.Println("\nLoaded Investigator from Database:")
-	loadedInvestigator.Print()
 }
