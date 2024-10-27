c33fac8ba6e3971cec200ebfb076dc8153998489
pedro-git-projects <pedro.coding.contact@gmail.com>
2024-10-27 14:57:36 -0300

    [ref] removing unused form field

--- src/gui/forms/characteristics.go
+++ src/gui/forms/characteristics.go
@@ -11,31 +11,31 @@ import (
 )
 
 type CharacteristicsForm struct {
-	StrEntry     *widget.Entry
-	DexEntry     *widget.Entry
-	IntEntry     *widget.Entry
-	ConEntry     *widget.Entry
-	AppEntry     *widget.Entry
-	PowEntry     *widget.Entry
-	SizEntry     *widget.Entry
-	EduEntry     *widget.Entry
-	MoveEntry    *widget.Entry
+	StrEntry *widget.Entry
+	DexEntry *widget.Entry
+	IntEntry *widget.Entry
+	ConEntry *widget.Entry
+	AppEntry *widget.Entry
+	PowEntry *widget.Entry
+	SizEntry *widget.Entry
+	EduEntry *widget.Entry
+	//	MoveEntry    *widget.Entry
 	SubmitButton *widget.Button
 	OnSubmit     func(characteristics investigator.Characteristics)
 }
 
 func NewCharacteristicsForm(onSubmit func(characteristics investigator.Characteristics)) *CharacteristicsForm {
 	form := &CharacteristicsForm{
-		StrEntry:  widget.NewEntry(),
-		DexEntry:  widget.NewEntry(),
-		IntEntry:  widget.NewEntry(),
-		ConEntry:  widget.NewEntry(),
-		AppEntry:  widget.NewEntry(),
-		PowEntry:  widget.NewEntry(),
-		SizEntry:  widget.NewEntry(),
-		EduEntry:  widget.NewEntry(),
-		MoveEntry: widget.NewEntry(),
-		OnSubmit:  onSubmit,
+		StrEntry: widget.NewEntry(),
+		DexEntry: widget.NewEntry(),
+		IntEntry: widget.NewEntry(),
+		ConEntry: widget.NewEntry(),
+		AppEntry: widget.NewEntry(),
+		PowEntry: widget.NewEntry(),
+		SizEntry: widget.NewEntry(),
+		EduEntry: widget.NewEntry(),
+		// MoveEntry: widget.NewEntry(),
+		OnSubmit: onSubmit,
 	}
 
 	form.SubmitButton = widget.NewButton("Submit", form.handleSubmit)
@@ -52,15 +52,15 @@ func (f *CharacteristicsForm) handleSubmit() {
 	}
 
 	characteristics := investigator.Characteristics{
-		Str:  parsePoint(f.StrEntry),
-		Dex:  parsePoint(f.DexEntry),
-		Int:  parsePoint(f.IntEntry),
-		Con:  parsePoint(f.ConEntry),
-		App:  parsePoint(f.AppEntry),
-		Pow:  parsePoint(f.PowEntry),
-		Siz:  parsePoint(f.SizEntry),
-		Edu:  parsePoint(f.EduEntry),
-		Move: parsePoint(f.MoveEntry),
+		Str: parsePoint(f.StrEntry),
+		Dex: parsePoint(f.DexEntry),
+		Int: parsePoint(f.IntEntry),
+		Con: parsePoint(f.ConEntry),
+		App: parsePoint(f.AppEntry),
+		Pow: parsePoint(f.PowEntry),
+		Siz: parsePoint(f.SizEntry),
+		Edu: parsePoint(f.EduEntry),
+		// Move: parsePoint(f.MoveEntry),
 	}
 
 	characteristics.PrintCharacteristics()
@@ -80,7 +80,7 @@ func (f *CharacteristicsForm) Render() fyne.CanvasObject {
 		widget.NewLabel("Power (Pow)"), f.PowEntry,
 		widget.NewLabel("Size (Siz)"), f.SizEntry,
 		widget.NewLabel("Education (Edu)"), f.EduEntry,
-		widget.NewLabel("Movement (Move)"), f.MoveEntry,
+		// widget.NewLabel("Movement (Move)"), f.MoveEntry,
 		f.SubmitButton,
 	)
 }
